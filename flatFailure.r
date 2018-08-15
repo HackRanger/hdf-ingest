ba92ff3743cc309ed4d1b68b3116775a7d6ceca8
Subramanya Umashankar <subramanya-umashankar.joshi@scania.com>
2018-08-15 22:02:15 +0200

    datascience

---
+++ flatFailure.r
@@ -0,0 +1,45 @@
+library(zoo)
+setwd("C:\\workspace\\dataset1\\")
+colnames = c("datetime","machineID","comp")
+failuredata = read.table("machine_1_failures.csv",sep=",",header=FALSE,col.names=colnames)
+component_type = c("comp1","comp2","comp3","comp4")
+start <- as.POSIXlt("2015-07-01 06:00:00")
+end   <- as.POSIXlt("2015-08-30 06:00:00")
+dates <- seq(start, end, by=86400)
+
+datetimes = c()
+comp1s = c()
+comp2s = c()
+comp3s = c()
+comp4s = c()
+
+
+for ( date in dates )
+{
+  comp1 = 0
+  comp2 = 0
+  comp3 = 0
+  comp4 = 0
+  for(row in 1:nrow(failuredata)){
+    rowdate = as.POSIXlt(failuredata[row,"datetime"])
+    rowcomp = failuredata[row,"comp"]
+    if(rowdate == date){
+      if(rowcomp == "comp1"){
+        comp1 <- comp1 + 1
+      }else if(rowcomp == "comp2"){
+        comp2 <- comp2 + 1
+      }else if(rowcomp == "comp3"){
+        comp3 <- comp3 + 1
+      }else if(rowcomp == "comp4"){
+        comp4 <- comp4 + 1
+      } 
+    }
+  }
+  datetimes = c(datetimes,date)
+  comp1s = c(comp1s,comp1)
+  comp2s = c(comp2s,comp2)
+  comp3s = c(comp3s,comp3)
+  comp4s = c(comp4s,comp4)
+}
+f = data.frame(datetimes,comp1s,comp2s,comp3s,comp4s)
+head(f)
